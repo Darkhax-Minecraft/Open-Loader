fbe3edde7542a4bc24e48c474047317ad2e4352c
Tyler Hancock <darklime@live.ca>
2021-01-29 01:43:48 -0700

    Allow vanilla clients to connect to servers using this mod.

--- src/main/java/net/darkhax/openloader/OpenLoader.java
+++ src/main/java/net/darkhax/openloader/OpenLoader.java
@@ -1,14 +1,18 @@
 package net.darkhax.openloader;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod("openloader")
 public final class OpenLoader {
@@ -16,6 +20,8 @@ public final class OpenLoader {
     public static final Logger LOGGER = LogManager.getLogger("Open Loader");
     
     public OpenLoader() {
+        
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         
         MinecraftForge.EVENT_BUS.addListener(this::onServerStart);
         
