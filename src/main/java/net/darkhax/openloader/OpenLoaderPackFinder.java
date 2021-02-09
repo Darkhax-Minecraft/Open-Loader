f3e7a6efaf48e83a950670f08c28a85d4a0a098e
Tyler Hancock <darklime@live.ca>
2021-02-09 13:02:49 -0700

    Allow packs to define their own builtin and priority. #8

--- src/main/java/net/darkhax/openloader/OpenLoaderPackFinder.java
+++ src/main/java/net/darkhax/openloader/OpenLoaderPackFinder.java
@@ -7,6 +7,8 @@ import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
 
 import com.google.common.base.Supplier;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 
 import net.minecraft.resources.FilePack;
 import net.minecraft.resources.FolderPack;
@@ -15,9 +17,13 @@ import net.minecraft.resources.IPackNameDecorator;
 import net.minecraft.resources.IResourcePack;
 import net.minecraft.resources.ResourcePackInfo;
 import net.minecraft.resources.ResourcePackInfo.IFactory;
+import net.minecraft.resources.ResourcePackInfo.Priority;
+import net.minecraft.resources.data.IMetadataSectionSerializer;
+import net.minecraft.util.JSONUtils;
 
 public final class OpenLoaderPackFinder implements IPackFinder {
     
+    private static final IMetadataSectionSerializer<PackMeta> META_SERIALIZER = new PackMetaSerializer();
     public static final OpenLoaderPackFinder DATA = new OpenLoaderPackFinder(Type.DATA);
     public static final OpenLoaderPackFinder RESOUCE = new OpenLoaderPackFinder(Type.RESOURCES);
     
@@ -57,9 +63,13 @@ public final class OpenLoaderPackFinder implements IPackFinder {
             if (isFilePack || isFolderPack) {
                 
                 final String packName = this.type.path + "/" + packCandidate.getName();
+                final Supplier<IResourcePack> pack = this.getAsPack(packCandidate);
+                final PackMeta meta = this.getPackMeta(packName, pack);
                 
                 OpenLoader.LOGGER.info("Loading {} {}.", this.type.displayName, packName);
-                final ResourcePackInfo packInfo = ResourcePackInfo.createResourcePack(packName, true, this.getAsPack(packCandidate), factory, ResourcePackInfo.Priority.TOP, IPackNameDecorator.PLAIN);
+                OpenLoader.LOGGER.debug("Loading {} with builtin={} priority={}", packName, meta.isBuiltIn, meta.priority);
+                
+                final ResourcePackInfo packInfo = ResourcePackInfo.createResourcePack(packName, meta.isBuiltIn, pack, factory, meta.priority, IPackNameDecorator.PLAIN);
                 
                 if (packInfo != null) {
                     
@@ -72,6 +82,27 @@ public final class OpenLoaderPackFinder implements IPackFinder {
                 OpenLoader.LOGGER.error("Failed to load {} from {}. Archive packs must be zips. Folder packs must have a valid pack.mcmeta file.", this.type.displayName, packCandidate.getAbsolutePath());
             }
         }
+    }
+    
+    private PackMeta getPackMeta (String packName, Supplier<IResourcePack> pack) {
+        
+        try (IResourcePack packData = pack.get()) {
+            
+            final PackMeta meta = packData.getMetadata(META_SERIALIZER);
+            
+            if (meta != null) {
+                
+                return meta;
+            }
+        }
+        
+        catch (final IOException e) {
+            
+            OpenLoader.LOGGER.warn("Could not load pack meta from {}!", packName);
+            OpenLoader.LOGGER.catching(e);
+        }
+        
+        return new PackMeta(true, Priority.TOP);
     }
     
     private Supplier<IResourcePack> getAsPack (File file) {
@@ -133,6 +164,42 @@ public final class OpenLoaderPackFinder implements IPackFinder {
             this.displayName = name;
             this.path = path;
             this.enabled = enabled;
+        }
+    }
+    
+    static class PackMetaSerializer implements IMetadataSectionSerializer<PackMeta> {
+        
+        @Override
+        public String getSectionName () {
+            
+            return "openloader";
+        }
+        
+        @Override
+        public PackMeta deserialize (JsonObject json) {
+            
+            final boolean builtin = JSONUtils.getBoolean(json, "builtin", true);
+            final String priorityName = JSONUtils.getString(json, "priority", "top").toLowerCase();
+            final Priority priority = "top".equalsIgnoreCase(priorityName) ? Priority.TOP : "bottom".equalsIgnoreCase(priorityName) ? Priority.BOTTOM : null;
+            
+            if (priority == null) {
+                
+                throw new JsonParseException("Expected priority to be \"top\" or \"bottom\". " + priorityName + " is not a valid value!");
+            }
+            
+            return new PackMeta(builtin, priority);
+        }
+    }
+    
+    static class PackMeta {
+        
+        public final boolean isBuiltIn;
+        public final Priority priority;
+        
+        public PackMeta(boolean isBuiltIn, Priority priority) {
+            
+            this.isBuiltIn = isBuiltIn;
+            this.priority = priority;
         }
     }
 }
